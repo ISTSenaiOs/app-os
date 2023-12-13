4d0e364c520df8b3e43d6d55b551e323ffd70ca2
chriaalonso <chriaalonso@gmail.com>
2023-12-13 15:20:18 -0300

    Telas de cadastro

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+export const safelist = [{ pattern: /^.*$/ }];
+
+/*@type {import('tailwindcss').Config}
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}*/
+
